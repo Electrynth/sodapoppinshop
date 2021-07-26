acfa4cd0e1015da132c24b812fe65d116110dbeb
Nick Brown <ncbrown@cpp.edu>
2021-07-26 14:01:49 -0700

    Add google analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import App from './App';
+
+const TRACKING_ID = "UA-96951707-5";
+ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.render(
   <App />,
